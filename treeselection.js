38ecbb9e382844cee5a9e1e7aa2e6cda1717d731
Sepand Parhami <sepandparhami@gmail.com>
2018-04-24 21:04:00 -0700

    Change to use es6 instead of html imports.

--- treeselection.js
+++ treeselection.js
@@ -15,200 +15,202 @@
  * limitations under the License.
  */
 
-const attachTreeSelection = (function() {
-  const filter = {
-    acceptNode: function(node) {
-      const role = node.getAttribute('role');
+const filter = {
+  acceptNode: function(node) {
+    const role = node.getAttribute('role');
 
-      if (role === 'treeitem') {
-        return NodeFilter.FILTER_ACCEPT;
-      }
-
-      return NodeFilter.FILTER_SKIP;
-    }
-  };
-
-  function isExpandedGroup(el) {
-    return el.getAttribute('aria-expanded') === 'true';
-  }
-
-  function isGroup(el) {
-    return el.hasAttribute('aria-expanded');
-  }
-
-  function setExpanded(el, expanded) {
-    el.setAttribute('aria-expanded', expanded);
-  }
-
-  function setSelected(el, selected) {
-    const evtName = selected ? 'selected' : 'deselected';
-
-    el.tabIndex = selected ? 0 : -1;
-    el.setAttribute('aria-selected', selected);
-    el.dispatchEvent(new CustomEvent(evtName, {
-      bubbles: true 
-    }));
-  }
-
-  function toggleExpanded(el) {
-    if (isGroup(el)) {
-      setExpanded(el, !isExpandedGroup(el));
-    }
-  }
-
-  function first(walker) {
-    walker.currentNode = walker.root;
-    walker.nextNode();
-    walker.currentNode.focus();
-  }
-
-  function last(walker) {
-    walker.currentNode = walker.root;
-    while(walker.lastChild() && isExpandedGroup(walker.currentNode));
-    walker.currentNode.focus();
-  }
-
-  function handleBoundary(walker, forward, wrap) {
-    if (forward != wrap) {
-      last(walker);
-    } else {
-      first(walker);
-    }
-  }
-
-  function next(walker, wrap, trap) {
-    if (walker.currentNode === walker.root) {
-      first(walker);
-    } else if (trap) {
-      if (!walker.nextSibling() && wrap) {
-        while(walker.previousSibling());
-      }
-    } else if (isExpandedGroup(walker.currentNode)) {
-      walker.firstChild();
-    } else {
-      while(!walker.nextSibling()) {
-        if (!walker.parentNode()) {
-          handleBoundary(walker, true, wrap);
-          break;
-        }
-      }
+    if (role === 'treeitem') {
+      return NodeFilter.FILTER_ACCEPT;
     }
 
-    walker.currentNode.focus();
+    return NodeFilter.FILTER_SKIP;
   }
+};
 
-  function previous(walker, wrap, trap) {
-    if (walker.currentNode === walker.root) {
-      last(walker);
-    } else if (trap) {
-      if (!walker.previousSibling() && wrap) {
-        while(walker.nextSibling());
-      }
-    } else if (!walker.previousSibling()) {
+function isExpandedGroup(el) {
+  return el.getAttribute('aria-expanded') === 'true';
+}
+
+function isGroup(el) {
+  return el.hasAttribute('aria-expanded');
+}
+
+function setExpanded(el, expanded) {
+  el.setAttribute('aria-expanded', expanded);
+}
+
+function setSelected(el, selected) {
+  const evtName = selected ? 'selected' : 'deselected';
+
+  el.tabIndex = selected ? 0 : -1;
+  el.setAttribute('aria-selected', selected);
+  el.dispatchEvent(new CustomEvent(evtName, {
+    bubbles: true 
+  }));
+}
+
+function toggleExpanded(el) {
+  if (isGroup(el)) {
+    setExpanded(el, !isExpandedGroup(el));
+  }
+}
+
+function first(walker) {
+  walker.currentNode = walker.root;
+  walker.nextNode();
+  walker.currentNode.focus();
+}
+
+function last(walker) {
+  walker.currentNode = walker.root;
+  while(walker.lastChild() && isExpandedGroup(walker.currentNode));
+  walker.currentNode.focus();
+}
+
+function handleBoundary(walker, forward, wrap) {
+  if (forward != wrap) {
+    last(walker);
+  } else {
+    first(walker);
+  }
+}
+
+function next(walker, wrap, trap) {
+  if (walker.currentNode === walker.root) {
+    first(walker);
+  } else if (trap) {
+    if (!walker.nextSibling() && wrap) {
+      while(walker.previousSibling());
+    }
+  } else if (isExpandedGroup(walker.currentNode)) {
+    walker.firstChild();
+  } else {
+    while(!walker.nextSibling()) {
       if (!walker.parentNode()) {
-        handleBoundary(walker, false, wrap);
+        handleBoundary(walker, true, wrap);
+        break;
       }
-    } else {
-      while(isExpandedGroup(walker.currentNode) && walker.lastChild());
     }
-
-    walker.currentNode.focus();
   }
 
-  function rightArrow(walker) {
-    if (!isGroup(walker.currentNode)) {
+  walker.currentNode.focus();
+}
+
+function previous(walker, wrap, trap) {
+  if (walker.currentNode === walker.root) {
+    last(walker);
+  } else if (trap) {
+    if (!walker.previousSibling() && wrap) {
+      while(walker.nextSibling());
+    }
+  } else if (!walker.previousSibling()) {
+    if (!walker.parentNode()) {
+      handleBoundary(walker, false, wrap);
+    }
+  } else {
+    while(isExpandedGroup(walker.currentNode) && walker.lastChild());
+  }
+
+  walker.currentNode.focus();
+}
+
+function rightArrow(walker) {
+  if (!isGroup(walker.currentNode)) {
+    return;
+  }
+
+  setExpanded(walker.currentNode, true);
+  walker.firstChild();
+  walker.currentNode.focus();
+}
+
+function leftArrow(walker) {
+  if (isExpandedGroup(walker.currentNode)) {
+    setExpanded(walker.currentNode, false);
+  } else {
+    walker.parentNode();
+  }
+
+  walker.currentNode.focus();
+}
+
+function expandAll(walker) {
+  const groups = walker.root.querySelectorAll('[aria-expanded]');
+  for (let i = 0; i < groups.length; i += 1) {
+    setExpanded(groups[i], true);
+  }
+}
+
+function attachTreeSelection(el, config = {}) {
+  const { wrap = false, trap = false } = config;
+  const walker = document.createTreeWalker(el, NodeFilter.SHOW_ELEMENT, filter);
+  let selected = walker.root;
+
+  el.addEventListener('keydown', function(e) {
+    let handled = true;
+
+    switch (e.keyCode) {
+      case 13: // enter
+        toggleExpanded(walker.currentNode);
+        break;
+      case 35: // end
+        last(walker);
+        break;
+      case 36: // home
+        first(walker);
+        break;
+      case 37: // left
+        leftArrow(walker);
+        break;
+      case 38: // up
+        previous(walker, wrap, trap);
+        break;
+      case 39: // right
+        rightArrow(walker);
+        break;
+      case 40: // down
+        next(walker, wrap, trap);
+        break;
+      case 106: // keypad *
+        expandAll(walker);
+        break;
+      default:
+        handled = false;
+        break;
+    }
+
+    if (handled) {
+      e.preventDefault();
+    }
+  });
+
+  el.addEventListener('focus', function(e) {
+    const item = e.target.closest('[role="treeitem"]');
+
+    if (!item || item === selected) {
       return;
     }
 
-    setExpanded(walker.currentNode, true);
-    walker.firstChild();
-    walker.currentNode.focus();
-  }
-
-  function leftArrow(walker) {
-    if (isExpandedGroup(walker.currentNode)) {
-      setExpanded(walker.currentNode, false);
-    } else {
-      walker.parentNode();
+    if (selected) {
+      setSelected(selected, false);
     }
 
-    walker.currentNode.focus();
-  }
+    walker.currentNode = selected = item;
+    setSelected(selected, true);
+  }, true);
 
-  function expandAll(walker) {
-    const groups = walker.root.querySelectorAll('[aria-expanded]');
-    for (let i = 0; i < groups.length; i += 1) {
-      setExpanded(groups[i], true);
+  el.addEventListener('dblclick', function(e) {
+    const item = e.target.closest('[role="treeitem"]');
+
+    if (!item) {
+      return;
     }
-  }
 
-  return function(el, config = {}) {
-    const { wrap = false, trap = false } = config;
-    const walker = document.createTreeWalker(el, NodeFilter.SHOW_ELEMENT, filter);
-    let selected = walker.root;
+    toggleExpanded(item);      
+    e.stopPropagation();
+  });
+}
 
-    el.addEventListener('keydown', function(e) {
-      let handled = true;
-
-      switch (e.keyCode) {
-        case 13: // enter
-          toggleExpanded(walker.currentNode);
-          break;
-        case 35: // end
-          last(walker);
-          break;
-        case 36: // home
-          first(walker);
-          break;
-        case 37: // left
-          leftArrow(walker);
-          break;
-        case 38: // up
-          previous(walker, wrap, trap);
-          break;
-        case 39: // right
-          rightArrow(walker);
-          break;
-        case 40: // down
-          next(walker, wrap, trap);
-          break;
-        case 106: // keypad *
-          expandAll(walker);
-          break;
-        default:
-          handled = false;
-          break;
-      }
-
-      if (handled) {
-        e.preventDefault();
-      }
-    });
-
-    el.addEventListener('focus', function(e) {
-      const item = e.target.closest('[role="treeitem"]');
-
-      if (!item || item === selected) {
-        return;
-      }
-
-      if (selected) {
-        setSelected(selected, false);
-      }
-
-      walker.currentNode = selected = item;
-      setSelected(selected, true);
-    }, true);
-
-    el.addEventListener('dblclick', function(e) {
-      const item = e.target.closest('[role="treeitem"]');
-
-      if (!item) {
-        return;
-      }
-
-      toggleExpanded(item);      
-      e.stopPropagation();
-    });
-  }
-})();
+export {
+  attachTreeSelection,
+};
